2b95029c3220389eeef6808dc88a916474addbdf
MacKenzie Joyal <mackenzie.joyal@email.com>
2020-11-17 04:22:04 +0000

    added spec/models

---
+++ spec/models/spec/product_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Spec::Product, type: :model do
+
+  describe 'Validations' do 
+    it "is not valid without a name" do
+      
+    end
+
+    it "is not valid without a name" do
+    end
+
+    it "is not valid without a name" do
+    end
+
+    it "is not valid without a name" do
+    end
+
+  end
+end
